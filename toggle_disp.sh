8a7ae84c52d1eae9979ffc1a353c657bf161c4bc
Oliver Stollmann <oliver@stollmann.net>
2012-08-23 13:38:50 +0200

    pre-win

--- toggle_disp.sh
+++ toggle_disp.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 INTERNAL="LVDS1"
-EXTERNAL="HDMI1"
+EXTERNAL="VGA1"
 
 if (xrandr | grep "$EXTERNAL" | grep "+")
     then
     xrandr --output $EXTERNAL --off --output $INTERNAL --auto
+    feh --bg-scale /home/ost/media/images/wallpapers/beach1.jpg &
     else
         if (xrandr | grep "$EXTERNAL" | grep "connected")
             then
             xrandr --output $INTERNAL --off --output $EXTERNAL --auto
+            feh --bg-scale /home/ost/media/images/wallpapers/beach1.jpg &
         fi
 fi
